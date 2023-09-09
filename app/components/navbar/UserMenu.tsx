5ea71f5af58a7f565eddc5e5d1c0d19528438a98
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2023-09-09 19:30:39 +0530

    Put your home form

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,6 +7,7 @@ import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/app/types";
+import useRentModal from "@/app/hooks/useRentModal";
 
 interface UserMenuProps{
   currentUser?: SafeUser| null;
@@ -15,17 +16,29 @@ interface UserMenuProps{
 const UserMenu:React.FC<UserMenuProps> = ({currentUser}) => {
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
+  const rentModal = useRentModal()
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
 
+  const onRent = useCallback(
+    () => {
+      if(!currentUser){
+        return loginModal.onOpen()
+      }
+      rentModal.onOpen()
+    },
+    [currentUser,loginModal,rentModal],
+  )
+  
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div
-          onClick={() => {}}
+          onClick={onRent}
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
         >
           HouseSpot your home
@@ -49,7 +62,7 @@ const UserMenu:React.FC<UserMenuProps> = ({currentUser}) => {
             <MenuItem onClick={()=>{}} label="My favorites" />
             <MenuItem onClick={()=>{}} label="My reservations" />
             <MenuItem onClick={()=>{}} label="My properties" />
-            <MenuItem onClick={()=>{}} label="Airbnb my home" />
+            <MenuItem onClick={rentModal.onOpen} label="Airbnb my home" />
             <hr/>
             <MenuItem onClick={()=>{signOut()}} label="Logout" />
           </>
