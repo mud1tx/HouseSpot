dc6bad3b57134f46809642474c04b23074812a9c
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2023-09-10 19:02:41 +0530

    Cmpleted Listing and some routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,10 @@ export default async function RootLayout({
         <RegisterModal />
         <RentModal/>
         <LoginModal/>
-        <Navbar currentUser = {currentUser}/>
-        {children}
+        <Navbar currentUser = {currentUser}/>  
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
       </body>
     </html>
   );
